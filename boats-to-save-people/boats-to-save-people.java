602c6709366e5e8a5cfd219a7c37fe65cccff154
Clement  Bowe <cbowe023@fiu.edu>
2021-06-13 10:30:57 -0400

    Time: 14 ms (87.33%), Space: 47.8 MB (63.25%) - LeetHub

---
+++ boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length-1;
+        int totalBoats = 0;
+        while(left <= right){
+            if(people[left]+people[right] <= limit)
+                left++;
+            totalBoats++;
+            right--; 
+        }
+        return totalBoats; 
+    }
+}
