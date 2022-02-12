9b6ec83774bb638a6d54b30809ebddc51d2fc0e6
Clement  Bowe <cbowe023@fiu.edu>
2022-02-12 16:17:46 -0500

    Time: 18 ms (34.17%), Space: 47.6 MB (55.37%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,36 +1,16 @@
 class Solution {
-    
     public int findPairs(int[] nums, int k) {
         int count = 0;
-        Arrays.sort(nums);
-        HashMap<Integer, Set<Integer>> map = new HashMap<>();
-        for(int i :nums){
-            map.put(i, new HashSet<>());
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i,0)+1);
         }
         
-        for(int i = 0; i < nums.length-1; i++){
-            
-            if(i > 0 && nums[i-1] == nums[i]){
-                continue;
-            }
-            int left = i+1;
-            int right = nums.length-1;
-            
-            while(left <= right){
-                int mid = (left + right)/2;
-                if(nums[mid]-nums[i] < k) {
-                    // System.out.println("I'm too small " + (nums[i] - nums[mid]));
-                    left = mid+1;
-                } else {
-                    // System.out.println("I'm too large or equal " + (nums[mid] - nums[mid]));
-                    if(nums[mid]-nums[i] == k && map.get(nums[i]).add(nums[mid])){
-                        count++;
-                    }
-                    right = mid-1;
-                }
+        for(int key : map.keySet()){
+            if((k > 0 && map.containsKey(key+k)) || (k == 0 && map.get(key) > 1)){
+                count++;
             }
         }
-        
         return count;
     }
 }
