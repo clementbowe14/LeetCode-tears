1a40df687ddb5eb27d21cca5160ff84ad6d946ca
Clement  Bowe <cbowe023@fiu.edu>
2022-04-12 16:22:29 -0400

    Time: 0 ms (100.00%), Space: 2 MB (20.04%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.go
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.go
@@ -1,10 +1,20 @@
+
+/*
+ 2 - 6  2, 3, 4, 5 6 low - high / 2
+ 3 - 7 3, 4 5, 6,7 low - high / 2 + 1
+ 8 - 9 8 9 low - high / 2
+ 21 24 21 22 23 24 low - high / 2
+*/
 func countOdds(low int, high int) int {
-    count := 0
-    for i := low; i <= high; i++ {
-        if(i % 2 != 0) {
-            count++;
-        }
+    interval := (high - low)
+    if(interval % 2 == 0 && low % 2 == 0) {
+        return interval / 2 
+    } else if(interval % 2 == 0) {
+        return interval / 2 + 1
+    } else if (interval == 1) {
+        return interval
     }
-    return count
+    
+    return interval / 2 + 1
 }
       
