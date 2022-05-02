ce72fd982561ae41ed1c96428f7a94e9d6f2f907
Clement  Bowe <cbowe023@fiu.edu>
2022-05-02 12:49:48 -0400

    Time: 4 ms (20.82%), Space: 84.4 MB (15.68%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices == null || prices.length <= 1 ) {
+        if(prices.length <= 1 ) {
             return 0;
         }
         int max = 0;
         int currentMin = prices[0];
-        for(int i = 0; i < prices.length; i++) {
+        for(int i = 1; i < prices.length; i++) {
             max = Math.max(prices[i] - currentMin, max);
             currentMin = Math.min(currentMin, prices[i]);
         }
