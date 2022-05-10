be129064ea348adf5ba4afc14918d01a318ff303
Clement  Bowe <cbowe023@fiu.edu>
2022-05-10 00:08:51 -0400

    Time: 3 ms (92.47%), Space: 50.5 MB (92.26%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,31 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int n = grid[0].length;
-        int m = grid.length;
-        boolean[][]visited = new boolean[grid.length][grid[0].length];
-        int numIslands = 0; 
+        int A = 0;
         for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
-                if(!visited[i][j] && grid[i][j] =='1'){
-                    numIslands++;
-                    dfs(i, j, visited, grid, m, n);
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    A++;
+                    dfs(grid, i, j);
                 }
-                visited[i][j] = true;
             }
         }
-        return numIslands;
+        
+        return A;
     }
-    public void dfs(int row, int col, boolean[][] visited, char[][] grid, int endRow, int endCol){
-        if(row >= endRow || row < 0 || col >= endCol || col < 0 || visited[row][col]
+    
+    public void dfs(char[][] grid, int row, int col) {
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length
           || grid[row][col] == '0'){
             return;
         }
-        visited[row][col] = true; 
         
-        dfs(row+1, col, visited, grid, endRow, endCol);
-        dfs(row-1, col, visited, grid, endRow, endCol);
-        dfs(row, col+1, visited, grid, endRow, endCol);
-        dfs(row, col-1, visited, grid, endRow, endCol);
+        grid[row][col] = '0';
+        
+        dfs(grid, row-1, col);
+        dfs(grid, row+1, col);
+        dfs(grid, row, col-1);
+        dfs(grid, row, col+1);
         
     }
 }
