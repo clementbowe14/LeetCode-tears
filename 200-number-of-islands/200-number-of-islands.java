3a82468330241e7e59e317e6781e9a85fbadf11e
Clement  Bowe <cbowe023@fiu.edu>
2022-05-10 00:09:23 -0400

    Time: 3 ms (92.47%), Space: 50.5 MB (92.26%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -9,18 +9,14 @@ class Solution {
                 }
             }
         }
-        
         return A;
     }
-    
     public void dfs(char[][] grid, int row, int col) {
         if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length
           || grid[row][col] == '0'){
             return;
-        }
-        
+        }  
         grid[row][col] = '0';
-        
         dfs(grid, row-1, col);
         dfs(grid, row+1, col);
         dfs(grid, row, col-1);
