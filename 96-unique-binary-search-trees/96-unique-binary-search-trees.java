f4762ebd5ad42fc1ff578208260b3430897c86dd
Clement  Bowe <cbowe023@fiu.edu>
2022-05-29 16:09:35 -0400

    Time: 0 ms (100.00%), Space: 40.8 MB (45.33%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,13 +1,14 @@
 class Solution {
     public int numTrees(int n) {
-        int[] res = new int[n+1];
-        res[0] = 1;
-        res[1] = 1;
-        for(int i = 2; i <= n; i++) {
-            for(int j = 0; j < i; j++) {
-                res[i] += res[j] * res[i-j-1];
+        int[] trees = new int[n+1];
+        trees[0] = 1;
+        trees[1] = 1;
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j <=i; j++){
+                trees[i] += trees[j-1] * trees[i-j];
             }
         }
-        return res[n];
+        
+        return trees[n];
     }
 }
