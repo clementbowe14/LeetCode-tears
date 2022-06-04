eb03454d720aae91cb9b4ba9e752ea0c4311e19c
Clement  Bowe <cbowe023@fiu.edu>
2022-06-04 07:22:33 -0400

    Time: 8 ms (78.03%), Space: 43.4 MB (90.52%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,33 +1,33 @@
 class Solution {
-    
-    int [][] coords = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int[][] coords = {{0,1}, {0, -1}, {1,0}, {-1, 0}};
     public int nearestExit(char[][] maze, int[] entrance) {
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{entrance[0], entrance[1], 0});
+        int n = maze.length;
+        int m = maze[0].length;
+        
         boolean[][] visited = new boolean[maze.length][maze[0].length];
         visited[entrance[0]][entrance[1]] = true;
-        
         while(!queue.isEmpty()){
             int[] node = queue.poll();
             int row = node[0];
             int col = node[1];
             int steps = node[2];
-            
-            if(row == 0 || col == 0 || row == maze.length-1 || col == maze[0].length-1){
-                if(steps > 0)
-                    return steps;
+            if(steps > 0 && (row == 0 || row == n-1 || col == 0 || col == m-1)){
+                return steps;
             }
-            
-            for(int i = 0; i < coords.length; i++){
-                int nextRow = row + coords[i][0];
-                int nextCol = col + coords[i][1];
-                if(nextRow >= 0 && nextRow < maze.length && nextCol >= 0 && nextCol < maze[0].length && maze[nextRow][nextCol] != '+' && !visited[nextRow][nextCol]){
+            for(int [] coord : coords){
+                int nextRow = coord[0] + row;
+                int nextCol = coord[1] + col;
+                int nextSteps = 1 + steps;
+                if(nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < m && !visited[nextRow][nextCol] && maze[nextRow][nextCol] == '.'){
                     visited[nextRow][nextCol] = true;
-                    queue.add(new int[]{nextRow, nextCol, steps+1});
+                    queue.add(new int[]{nextRow, nextCol, nextSteps});
                 }
             }
         }
         
         return -1;
+        
     }
 }
