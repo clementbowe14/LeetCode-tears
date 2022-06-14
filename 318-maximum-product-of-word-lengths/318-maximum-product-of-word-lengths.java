c56666e102488c6ba4a7d4cec1b3dac5fcfe066f
Clement  Bowe <cbowe023@fiu.edu>
2022-06-14 06:44:49 -0400

    Time: 24 ms (47.20%), Space: 53.7 MB (20.64%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -5,14 +5,16 @@ class Solution {
         int ans = 0;
         for(String word : words){
             for(char letter : word.toCharArray()){
-                masks[idx] |= 1 <<(letter-'a');
+                masks[idx] |= 1 << letter -'a';
             }
             idx++;
         }
+        
         for(int i = 0; i < masks.length-1; i++){
             for(int j = i+1; j < masks.length; j++){
-                if((masks[i] & masks[j]) == 0)
-                    ans = Math.max(words[i].length()*words[j].length(), ans);
+                if((masks[i] & masks[j]) == 0){
+                    ans = Math.max(words[i].length() * words[j].length(), ans);
+                }
             }
         }
         
