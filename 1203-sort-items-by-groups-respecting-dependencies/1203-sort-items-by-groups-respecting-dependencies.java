b25d7ba3280011c55974b94b89a7e2b7c3b91ee5
Clement  Bowe <cbowe023@fiu.edu>
2022-06-14 06:32:52 -0400

    Time: 28 ms (96.00%), Space: 56.7 MB (97.60%) - LeetHub

--- 1203-sort-items-by-groups-respecting-dependencies/1203-sort-items-by-groups-respecting-dependencies.java
+++ 1203-sort-items-by-groups-respecting-dependencies/1203-sort-items-by-groups-respecting-dependencies.java
@@ -1,101 +1,86 @@
 class Solution {
-    boolean canSort;    
-    public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
-        List<List<Integer>> items = new ArrayList<>();
-        List<List<Integer>> groups = new ArrayList<>();
-        List<List<Integer>> groupsToItems = new ArrayList<>();
-        List<Integer> sortedItems = new ArrayList<>();
-        List<Integer> sortedGroups = new ArrayList<>();
-        int [] ans = new int[n];
-        canSort = true;
+    
+    public void topSort(List<List<Integer>> graph, int[] indegree, List<Integer> order){
+        Queue<Integer> queue = new LinkedList<>();
         
+        for(int i = 0; i < indegree.length; i++){
+            if(indegree[i] == 0){
+                queue.add(i);
+            }
+        }
+        
+        while(!queue.isEmpty()){
+                int node = queue.poll();
+                order.add(node);
+                for(int val: graph.get(node)){
+                    indegree[val]--;
+                    if(indegree[val] == 0)
+                        queue.add(val);
+                }
+            }
+            
+    }
+    public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
+        List<Integer> groupOrder = new ArrayList<>();
+        List<Integer> itemOrder = new ArrayList<>();
+        List<List<Integer>> groupToItemOrder = new ArrayList<>();
+        List<List<Integer>> itemGraph = new ArrayList<>();
+        List<List<Integer>> groupGraph = new ArrayList<>();
         
         for(int i = 0; i < group.length; i++){
             if(group[i] == -1){
                 group[i] = m++;
             }
-            items.add(new ArrayList<>());
         }
         
-       for(int i = 0; i < m; i++){
-           groups.add(new ArrayList<>());
-           groupsToItems.add(new ArrayList<>());
-       }
-        
-        for(int i = 0; i < n; i++){
-            List<Integer> neighbors = beforeItems.get(i);
-            for(int neighbor : neighbors){
-                if(group[neighbor] != group[i]){
-                    groups.get(group[i]).add(group[neighbor]);
-                }
-            }
+        for(int i = 0; i < m; i++){
+            groupGraph.add(new ArrayList<>());
+            groupToItemOrder.add(new ArrayList<>());
         }
         
         for(int i = 0; i < n; i++){
-            List<Integer> neighbors = beforeItems.get(i);
-            for(int neighbor : neighbors){
-                items.get(i).add(neighbor);
-            }
+            itemGraph.add(new ArrayList<>());
         }
         
-        boolean[] onStackGroups = new boolean[m];
-        boolean[] visitedItems = new boolean[n];
-        boolean[] visitedGroups = new boolean[m];
-        boolean[] onStackItems = new boolean[n];
-        
-        for(int i = 0; i < m; i++) {
-            if(!visitedGroups[i]){
-                dfs(i, groups, sortedGroups, visitedGroups, onStackGroups);
-                if(!canSort){
-                    return new int[]{};
-                }
-            }
-        }
-        
-        for(int i = 0; i < n; i++){
-            if(!visitedItems[i]){
-                dfs(i, items, sortedItems, visitedItems, onStackItems);
-            }
-            if(!canSort){
-                return new int[]{};
-            }
-        }
-        
-        for(int i = 0; i < n; i++){
-                groupsToItems.get(group[sortedItems.get(i)]).add(sortedItems.get(i));
-        }
+        int[] itemDegree = new int[n];
+        int [] groupDegree = new int[m];
         
         int idx = 0;
-        for(int i = 0; i < m; i++){
-            List<Integer> lst = groupsToItems.get(sortedGroups.get(i));
-            for(int item : lst){
+        for(List<Integer> items : beforeItems){
+            for(int item : items){
+                itemGraph.get(item).add(idx);
+                itemDegree[idx]++;
+            }
+            idx++;
+        }
+        
+        for(int i = 0; i < group.length; i++){
+            int targetGroup = group[i];
+            for(int item : beforeItems.get(i)){
+                if(group[item] != targetGroup){
+                    groupGraph.get(group[item]).add(targetGroup);
+                    groupDegree[targetGroup]++;
+                }
+            }
+        }
+        
+        topSort(groupGraph, groupDegree, groupOrder);
+        topSort(itemGraph, itemDegree, itemOrder);
+        
+        if(itemOrder.size() < n || groupOrder.size() < m){
+            return new int[]{};
+        }
+        
+        for(int i = 0; i < itemOrder.size(); i++){
+            groupToItemOrder.get(group[itemOrder.get(i)]).add(itemOrder.get(i));
+        }
+        int [] ans = new int[n];
+        idx = 0;
+        for(int groups: groupOrder){
+            for(int item : groupToItemOrder.get(groups)){
                 ans[idx++] = item;
             }
         }
-        
         return ans;
-            
     }
-    
-    public void dfs(int index, List<List<Integer>> graph, List<Integer> topSort, boolean[] visited, boolean[] onStack){
-        if(visited[index])
-            return;
-        
-        onStack[index] = true;
-        List<Integer> neighbors = graph.get(index);
-        for(int neighbor : neighbors){
-            if(onStack[neighbor]){
-                canSort = false;
-                break;
-            }
-            
-            dfs(neighbor, graph, topSort, visited, onStack);
-        }
-        
-        onStack[index] = false;
-        topSort.add(index);
-        visited[index] = true;
-    }
-    
-    
 }
