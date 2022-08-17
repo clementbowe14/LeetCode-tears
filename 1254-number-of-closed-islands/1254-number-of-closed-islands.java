33f15648f77d796814b6f677bdf31e679c592ca3
Clement  Bowe <cbowe023@fiu.edu>
2022-08-17 09:07:07 -0400

    Time: 3 ms (64.12%), Space: 47.1 MB (32.98%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,46 +1,34 @@
 class Solution {
     public int closedIsland(int[][] grid) {
         boolean[][] visited = new boolean[grid.length][grid[0].length];
-        int m = grid.length-1;
-        int n = grid[0].length-1;
-        int res = 0;
+        int ans = 0;
         for(int i = 0; i < grid.length; i++){
-            if(grid[i][0] == 0 && !visited[i][0]) {
-                dfs(i, 0, grid, visited);
-            }
-            if(grid[i][n] == 0 && !visited[i][n]){
-                dfs(i, n, grid, visited);
-            }
-        }
-        for(int i = 0; i < n; i++){
-            if(grid[0][i] == 0 && !visited[0][i]){
-                dfs(0, i, grid, visited);
-            }
-            if(grid[m][i] == 0 && !visited[m][i]){
-                dfs(m, i, grid, visited);
-            }
-        }
-        for(int i = 1; i < grid.length-1; i++){
-            for(int j = 1; j < grid[0].length-1; j++){
-                if(grid[i][j] == 0 && !visited[i][j]){
-                    dfs(i, j, grid, visited);
-                    res++;
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 0 && !visited[i][j] && dfs(i, j, grid, visited)) {
+                    ans++;
                 }
             }
         }
-        return res;
+        return ans;
     }
     
-    public void dfs(int row, int col, int[][] grid, boolean[][] visited){
-        if(row < 0 || row > grid.length-1 || col < 0 || col > grid[0].length-1 || visited[row][col] || grid[row][col] == 1){
-            return;
+    public boolean dfs(int row, int col, int[][] grid, boolean[][] visited){
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+            return false;
+        } 
+        
+        if(grid[row][col] == 1 || visited[row][col]) {
+            return true;
         }
+        
         visited[row][col] = true;
-        int [][] coord = {{0,1},{0,-1},{1,0},{-1,0}};
-        for(int i = 0; i < coord.length; i++){
-            int newRow = row + coord[i][0];
-            int newCol = col + coord[i][1];
-            dfs(newRow, newCol, grid, visited);
-        }
+        
+        boolean up = dfs(row-1, col, grid, visited);
+        boolean down = dfs(row+1, col, grid, visited);
+        boolean left = dfs(row, col-1, grid, visited);
+        boolean right = dfs(row, col+1, grid, visited);
+        
+        return up && down && left && right;
+
     }
 }
