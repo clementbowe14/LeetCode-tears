9e339a2253eb634c954d0c99164c45f0ee0bb0a0
Clement  Bowe <cbowe023@fiu.edu>
2022-08-18 06:56:39 -0400

    Time: 89 ms (58.26%), Space: 88.4 MB (49.00%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,18 +1,22 @@
 class Solution {
     public int minSetSize(int[] arr) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        int[] freq = new int[arr.length+1];
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int val : arr){
             map.put(val, map.getOrDefault(val, 0)+1);
         }
         
         for(int key: map.keySet()){
-            queue.add(map.get(key));
+            freq[map.get(key)]++;
         }
         int total = 0;
         int count = 0;
+        int start = arr.length;
         while(count < arr.length/2){
-            count += queue.poll();
+            while(start > 0 && freq[start] == 0 )
+                start--;
+            freq[start]--;
+            count += start;
             total++;
         }
         return total;
