d6c8cbec0c6961919113c1b63f8cb0a41dd07e80
Clement  Bowe <cbowe023@fiu.edu>
2022-08-18 07:30:05 -0400

    Time: 0 ms (100.00%), Space: 41.6 MB (48.33%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -5,14 +5,10 @@ class Solution {
         }
         int [] dp = new int[nums.length];
         dp[0] = nums[0];
-        dp[1] = nums[1];
-        int max = Math.max(dp[0], dp[1]);
+        dp[1] = Math.max(nums[0], nums[1]);
         for(int i = 2; i < nums.length; i++){
-            for(int j = 0; j < i-1; j++){
-                dp[i] = Math.max(nums[i] + dp[j], dp[i]);
-            }
-            max = Math.max(dp[i], max);
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
         }
-        return max;
+        return dp[dp.length-1];
     }
 }
