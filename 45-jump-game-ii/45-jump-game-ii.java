417c35dc88f3eb0ab37e152882964be19a8d5a13
Clement  Bowe <cbowe023@fiu.edu>
2022-08-29 12:07:55 -0400

    Time: 2 ms (82.05%), Space: 49.1 MB (74.58%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,20 +1,24 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length < 2){
+        if(nums.length < 2)
             return 0;
-        }
-        int [] dp = new int[nums.length];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
         
-        for(int i = 1; i < nums.length; i++){
-            for(int j = 0; j < i; j++){
-                boolean canJump = nums[j] + j >= i;
-                if(canJump){
-                    dp[i] = Math.min(dp[j] + 1, dp[i]);
-                }
+        int currentMaxJump = 0;
+        int previousJump = 0;
+        int res = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            
+            currentMaxJump = Math.max(currentMaxJump, nums[i] + i);
+            if(i == previousJump){
+                res++;
+                previousJump = currentMaxJump;
             }
+            
+            if(previousJump >= nums.length-1)
+                return res;
         }
-        return dp[nums.length-1];
+        
+        return res;
     }
 }
