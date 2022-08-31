836f36f497f06ebf81e168f297bc69abf9f1c748
Clement  Bowe <cbowe023@fiu.edu>
2022-08-31 07:09:26 -0400

    Time: 1 ms (83.47%), Space: 42.5 MB (80.44%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || k == 0 || head.next == null){
+            return head;
+        }
+        ListNode curr = head;
+        int count = 0;
+        while(curr != null){
+            curr = curr.next;
+            count++;
+        }
+        curr = head; 
+        k = k % count;
+        while(k > 0){
+            ListNode p1 = null;
+            ListNode p2 = curr;
+            while(p2.next != null){
+                p1 = p2;
+                p2 = p2.next;
+            }
+            p1.next = p2.next;
+            p2.next = curr;
+            curr = p2;
+            k--;
+        }
+        return curr;
+    }
+}
