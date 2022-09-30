7f5a7d5df11bbb3a2e7233f2901ea70a380c7c7e
Clement  Bowe <cbowe023@fiu.edu>
2022-09-30 13:13:54 -0400

    Time: 104 ms (47.58%), Space: 81.6 MB (38.64%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,20 +1,61 @@
 class Solution {
+    static class UF {
+        private int [] id;
+        private int [] size;
+        
+        public UF(int n) {
+            this.id = new int[n];
+            this.size = new int[n];
+            for(int i = 0; i < n; i++) {
+                id[i] = i;
+                size[i] = 1;
+            }
+        }
+        
+        public int find(int index) {
+            if(index == id[index])
+                return index;
+            
+            return find(id[index]);
+        }
+        
+        public void union(int x, int y) {
+            int parentX = find(x);
+            int parentY = find(y);
+            id[parentX] = parentY;
+            size[parentY] += size[parentX];
+        }
+        
+        public int computeLargestSequence() {
+            int ans = 0;
+            for(int i = 0; i < size.length; i++) {
+                if(id[i] == i){
+                    ans = Math.max(ans, size[i]);
+                }
+            }
+            return ans;
+        }
+    }
     public int longestConsecutive(int[] nums) {
+        UF uf = new UF(nums.length);
         HashMap<Integer, Integer> map = new HashMap<>();
-        int ans = 0;
-        for(int num : nums) {
-            if(!map.containsKey(num)) {
-                int left = map.containsKey(num-1) ? map.get(num-1) : 0;
-                int right = map.containsKey(num+1) ? map.get(num+1) : 0;
-                int sum = left + right + 1;
-                map.put(num, sum);
-                ans = Math.max(sum, ans);
-                map.put(num + right, sum);
-                map.put(num - left, sum);
+        for(int i = 0; i < nums.length; i++) {
+            if(!map.containsKey(nums[i])) {
+                
+                if(map.containsKey(nums[i]-1)) {
+                    uf.union(i, map.get(nums[i]-1));
+                }
+                
+                if(map.containsKey(nums[i]+1)){
+                    uf.union(i, map.get(nums[i]+ 1));
+                }
+                
+                map.put(nums[i], i);
             } else {
                 continue;
             }
         }
-        return ans;
+        
+        return uf.computeLargestSequence();
     }
 }
